753623096adbb8fb4feee804ca0ca84d10e73a15
rdtungul <rosephdarl@gmail.com>
2023-08-21 14:34:52 +0800

    adding about and error content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React from 'react'
 
 export default function About() {
-  return <h1>about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut quaerat
+        quia, eum veniam labore distinctio ab sint corrupti temporibus adipisci
+        voluptatibus voluptas! Placeat error laudantium amet earum accusantium,
+        doloremque temporibus? Beatae, amet saepe obcaecati debitis
+        consequuntur, rerum necessitatibus, quis iste nulla sed et maiores aut?
+        Dolor quaerat architecto cumque ullam quibusdam beatae obcaecati, labore
+        magni, aliquam amet totam harum necessitatibus. Officia, laboriosam
+        iure, consectetur est repellendus omnis assumenda corrupti pariatur
+        architecto saepe quaerat rem eaque ea quia asperiores tempora vel!
+      </p>
+    </section>
+  )
 }
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,16 @@
-import React from "react";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Error() {
-  return <h1>error page</h1>;
+  return (
+    <section className="section error-page">
+      <div className="error-container">
+        <h1 className="section-title">error 404 not found.</h1>
+
+        <Link to="/">
+          <button className="btn btn-primary">back home</button>
+        </Link>
+      </div>
+    </section>
+  )
 }
